610810aa1390b090d767a8757c05865dd1812d84
Xuan <117747264+l33zard@users.noreply.github.com>
2026-06-22 16:28:35 +0800

    Add: meeting-schedule - submission-14

---
+++ Data Structures & Algorithms/meeting-schedule/submission-14.py
@@ -0,0 +1,17 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        hm = set()
+        for i in intervals:
+            for j in range(i.start, i.end):
+                if j in hm:
+                    return False
+                hm.add(j)
+        return True 
